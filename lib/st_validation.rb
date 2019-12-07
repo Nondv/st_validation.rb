6af7d606e6a75f91cead69910559010e43e015f7
Dmitry Non <mail@nondv.io>
2019-12-07 17:37:37 +0000

    use require_relative instead of require

--- lib/st_validation.rb
+++ lib/st_validation.rb
@@ -1,19 +1,27 @@
-require "st_validation/version"
-require "st_validation/errors"
-require 'st_validation/validator_factory'
+require 'set'
+
+require_relative "st_validation/version"
+require_relative "st_validation/errors"
+require_relative 'st_validation/validator_factory'
 
 Dir[File.join(__dir__, 'st_validation', 'validators', '*.rb')].each { |file| require file }
 
 module StValidation
   class << self
     def build(blueprint)
-      StValidation::ValidatorFactory.new(basic_transformations).build(blueprint)
+      basic_factory.build(blueprint)
     end
 
     def with_extra_transformations(*transformations)
       all_transformations = transformations + basic_transformations
       StValidation::ValidatorFactory.new(all_transformations)
     end
+
+    def basic_factory
+      StValidation::ValidatorFactory.new(basic_transformations)
+    end
+
+    private
 
     def basic_transformations
       [
@@ -23,8 +31,6 @@ module StValidation
         ->(bp, factory) { bp.is_a?(Array) && bp.size == 1 ? array_validator(bp[0], factory) : bp }
       ]
     end
-
-    private
 
     def class_validator(klass)
       Validators::ClassValidator.new(klass)
